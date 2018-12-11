50a7608bdb5d4637125365163fffc79594074ed9
Sean McGonagle <seanm.mcgonagle@gmail.com>
2018-12-11 17:59:01 -0500

    first written problem

---
+++ vara_varb.py
@@ -0,0 +1,15 @@
+varA = 12
+
+varB = 'a'
+
+if type(varA) == str or type(varB) == str:
+    print("string involved")
+
+elif varA > varB:
+    print("bigger")
+
+elif varA == varB:
+    print("equal")
+
+elif varA < varB:
+    print("smaller")
