7f70099df69f686c8aa66089a00c080cc34c8974
Sean McGonagle <seanm.mcgonagle@gmail.com>
2018-12-13 15:46:54 -0500

    example functions

---
+++ example_functions.py
@@ -0,0 +1,19 @@
+
+def is_even(i):
+
+    print ("hi")
+    return i % 2 == 0
+
+
+print(is_even(3))
+print(is_even(4))
+
+
+def square(x):
+    """x must be an int or float
+    returns the square of a number x
+    """
+    return x**2
+
+
+print(square(5))
